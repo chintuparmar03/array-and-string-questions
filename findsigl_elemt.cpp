409026c33a7f900e865f212beb797679dcef6890
chintu parmar <chintuparmar603@gmail.com>
2024-11-16 23:32:14 +0530

    array question

--- findsigl_elemt.cpp
+++ findsigl_elemt.cpp
@@ -1,17 +1,25 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int arr[5]={1,2,3,1,2};
-  int temp;
-  for(int i=0;i<=5;i++){
-    for(int j=1;j<5;j++){
-        if(arr[j]==arr[i]){
-              cout<<arr[j];
-        }
-        
-        
+  int arr[10]={1,2,3,1,2,4,5,4,7,7};
+  int sgl,count=0;
+  for(int i=0;i<10;i++){
+    count=0;
+    sgl=arr[i];
+    for(int j=0;j<10;j++){
+      
+      if(arr[j]==sgl&&i!=j){
+          count++;
+          
+      }
     }
+    if(count==0){
+            cout<<arr[i]<<" ";
+          }
+    
+   
   }
+  
 
   
   return 0;
