c6eea5cb17b9d4297020d1ba8c811ccee6941065
chintu parmar <chintuparmar603@gmail.com>
2024-11-28 21:01:04 +0530

    paranthisis

---
+++ valid_parenthisis.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+    string str;
+    cin>>str;
+    int n=str.length();
+     stack<char> st;
+     int i=0;
+     while(i<n){
+        if(str[i]=='('||str[i]=='['||str[i]=='{'){
+            st.push(str[i]);
+        }
+        i++;
+     }
+     for(int i=0;i<)
+              
+   return 0;
+}
